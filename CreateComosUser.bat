8474a1ffba88f2325148a56a9e02b5a262749861
trasba <t.rasbach@gmx.net>
2015-12-04 15:45:47 +0100

    Update CreateComosUser.bat
    This script is self elevating. A file calles getadmin.vbs is temporarily created in the %temp% folder of the user.

--- CreateComosUser.bat
+++ CreateComosUser.bat
@@ -31,8 +31,7 @@ REm set /p ln=Enter Last Name:
 REm set /p email=Enter Email:
 REm set /p gid=Enter logon:
 REm dsadd user "CN=%fn% %ln%,ou=Cirtix,DC=ft52,DC=lokal" -samid %gid% -pwd COMOS$2015 -mustchpwd yes -memberOf "cn=PCS7ESUSER,ou=Cirtix,DC=ft52,DC=lokal" "cn=ComosCirtixUser,ou=Cirtix,DC=ft52,DC=lokal" -fn %fn% -ln %ln% -display "%fn% %ln%" -email %email%@siemens.com -upn %gid%@ft52.lokal
+'create the user
 dsadd user "CN=%1 %2,ou=Cirtix,DC=ft52,DC=lokal" -samid %4 -pwd COMOS$2015 -mustchpwd yes -memberOf "cn=PCS7ESUSER,ou=Cirtix,DC=ft52,DC=lokal" "cn=ComosCirtixUser,ou=Cirtix,DC=ft52,DC=lokal" -fn %1 -ln %2 -display "%1 %2" -email %3@siemens.com -upn %4@ft52.lokal
 
-REM echo on
-REM echo %1
 pause
